db42f4a2635385321028de4940d0897d725d8e7a
Eva Innerlohninger <evainnerlohninger@MacBook-Pro-M1-von-Eva.local>
2021-04-26 08:37:19 +0200

    Szenenwechsel funktioniert in beiden Szenen

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class AddNumbers : MonoBehaviour
 {
-    
 
-   void Calculate(int a , int b)
+
+    public void Calculate(int a, int b)
     {
-if(a+b =9)
+        if (a + b == 9)
+            Debug.Log("answer is 9");
+
+        else
+        {
+         Debug.Log("answer is not 9");
     }
+
+   }
+
 }
