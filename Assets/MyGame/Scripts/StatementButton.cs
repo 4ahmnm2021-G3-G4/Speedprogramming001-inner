18094b2c2bd426930bb62ea74138a30ae4b3863a
Eva Innerlohninger <evainnerlohninger@MacBook-Pro-M1-von-Eva.local>
2021-04-26 09:36:44 +0200

    add scripts, don't work

---
+++ Assets/MyGame/Scripts/StatementButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatementButton : MonoBehaviour
+{
+    GameObject Wahr;
+  GameObject Falsch;
+    bool aButton = true;
+
+public void SwitchAnswer(){
+
+if(aButton)
+{
+Wahr.text = "Antworten sind wahr.";
+aButton = false;
+}
+else {
+Falsch.text = "Antworten sind falsch.";
+aButton = true;
+}
+
+
+}
+
+}
